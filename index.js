f6790bcc071d9ee30f67c39cafd9b2241ed9ae0a
Lucianna Tittle <lucianna.roset@gmail.com>
2022-08-10 08:47:22 -0500

    commit tree 3fa1f7bbbe06c72d71d8d88f544dac150d77ec33

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const num1 = 43;
+const num2 = 44;
+const num3 = 50;
+const num4 = 42;
+
+function distanceFromHqInBlocks() {
+  const blockCalc = num2 - num1;
+  const blockCalc2 = num3 - num2;
+  return blockCalc, blockCalc2;
+}
